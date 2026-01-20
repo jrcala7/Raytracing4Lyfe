33cd976781c38973fb4dd3cb743d50ff0372353d
JR <jrcala7@gmail.com>
2026-01-20 12:40:30 +0800

    Simplyiy rayhit

--- RaytracingForLife/RaytracingForLife.cpp
+++ RaytracingForLife/RaytracingForLife.cpp
@@ -35,17 +35,17 @@ void PrintTestImage() {
 
 double Hit_Sphere(const Point3& center, double radius, const Ray& r) {
 	Vec3 oc = center - r.origin();
-	auto a = dot(r.direction(), r.direction());
-	auto b = -2.0 * dot(r.direction(), oc);
-	auto c = dot(oc, oc) - radius * radius;
+	auto a = r.direction().length_squared();
+	auto h = dot(r.direction(), oc);
+	auto c = oc.length_squared() - (radius * radius);
 
-	auto discriminant = b * b - 4 * a * c;
+	auto discriminant = h * h - a * c;
 
 	if (discriminant < 0) {
 		return -1.0;
 	}
 	else {
-		return (-b - std::sqrt(discriminant)) / (2.0 * a);
+		return (h - std::sqrt(discriminant)) / (a);
 	}
 }
 
@@ -56,7 +56,6 @@ Color Ray_Color(const Ray& r) {
 		Vec3 N = unit_vector(r.at(t) - Vec3(0, 0, -1));
 		return 0.5 * Color(N.x() + 1, N.y() + 1, N.z() + 1);
 	}
-		
 
 	Vec3 Unit_Dir = unit_vector(r.direction());
 	auto a = 0.5 * (Unit_Dir.y() + 1.0);
